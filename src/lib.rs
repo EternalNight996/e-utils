294e128586035c90cb8e323fd8de7d7d0c68fb9a
Eternal <EternalNight996@gmail.com>
2022-08-13 00:18:52 +0800

    fix bug

--- src/lib.rs
+++ src/lib.rs
@@ -62,7 +62,7 @@
 #![doc(test(attr(allow(unused_variables), deny(warnings))))]
 // Rustc lints.
 #![deny(missing_docs, unused_imports)]
-// #![cfg_attr(doc_cfg, feature(doc_cfg))]
+#![cfg_attr(docsrs, feature(docsrs))]
 
 #[macro_use]
 mod cfgs;
